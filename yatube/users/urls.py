a9a0b8988b44de8ffdd42dc140d1326517469489
ReckeYi <79193494+ReckeYi@users.noreply.github.com>
2023-12-14 10:08:30 +0300

    + CreationForm + signup.html

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,6 +1,7 @@
 # Импортируем из приложения django.contrib.auth нужный view-класс
 from django.contrib.auth.views import LogoutView
 from django.urls import path
+from . import views
 
 app_name = 'users'
 
@@ -12,5 +13,12 @@ urlpatterns = [
         # Да, во view-классах так можно! Как их не полюбить.
         LogoutView.as_view(template_name='users/logged_out.html'),
         name='logout'
+    ),
+    # Полный адрес страницы регистрации - auth/signup/,
+    # но префикс auth/ обрабатывется в головном urls.py
+    path(
+        'signup/',
+        views.SignUp.as_view(),
+        name='signup'
     )
 ]
