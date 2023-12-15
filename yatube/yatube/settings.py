769da58ad680ba23df2890d301272eb32b8fdd8b
ReckeYi <79193494+ReckeYi@users.noreply.github.com>
2023-12-15 13:33:17 +0300

    + password_change/reset: fixes + filebased.EmailBackend: connected

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -115,6 +115,14 @@ LOGIN_REDIRECT_URL = 'posts:index'
 # LOGOUT_REDIRECT_URL = 'posts:index'
 
 
+# Email
+
+#  подключаем движок filebased.EmailBackend
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+# указываем директорию, в которую будут складываться файлы писем
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
