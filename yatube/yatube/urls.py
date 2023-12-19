e438312eaaae200440b3da7b6a6002213364a854
ReckeYi <79193494+ReckeYi@users.noreply.github.com>
2023-12-19 09:15:27 +0300

    + about

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('about/', include('about.urls', namespace='about')),
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls', namespace='users')),
     path('auth/', include('django.contrib.auth.urls')),
